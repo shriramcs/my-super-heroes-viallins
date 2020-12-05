1cdee105e7c0b6c0d951e550c9b58d13e50395aa
shriramcs <shriramcs@gmail.com>
2020-12-05 12:54:26 +0530

    added superheroes and supervillains using npm modules

---
+++ index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+let mySuperHero = superheroes.random();
+let mySuperVillain = supervillains.random();
+
+console.log(mySuperHero);
+console.log(mySuperVillain);
